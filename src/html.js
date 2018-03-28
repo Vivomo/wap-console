4ada589f2ccb2ca372c44e5f92390b46cf00e566
Vimo <929992114@qq.com>
2018-03-28 23:32:55 +0800

    update UI & fix scroll bug

--- src/html.js
+++ src/html.js
@@ -3,8 +3,8 @@ let wapConsoleHTML = `
 <div class="opt-wrap">
     <button class="btn run">Run</button>
     <button class="btn min">Min</button>
-    <button class="btn prev">Prev</button>
-    <button class="btn next">Next</button>
+    <button class="btn prev">↑</button>
+    <button class="btn next">↓</button>
     <button class="btn close fr">Close</button>
     <button class="btn clear fr">Clear</button>
 </div>
--- test/dist/wapConsole.js
+++ test/dist/wapConsole.js
@@ -1,9 +1,9 @@
 "use strict";
 
-var wapConsoleStyle = "\n.wap-console-wrap{position:fixed;z-index:9999;padding:2vw;top:2vw;left:2vw;right:2vw;bottom:2vw;background:rgba(0,0,0,0.5);border:2px solid #097125;color:#fff;overflow:hidden;box-sizing:border-box}.wap-console-wrap *{margin:0;padding:0;box-sizing:border-box}.wap-console-wrap.min{width:2vw;height:2vw}.wap-console-wrap.hide{display:none}.wap-console-wrap .fr{float:right}.wap-console-wrap .console-input{display:block;padding:1vw;width:100%;height:300px;color:#fff;background:transparent;border:0;border-top:2px solid #097125;outline:0;resize:none}.wap-console-wrap .console-input::placeholder{color:#fff}.wap-console-wrap .console-output .input-log,.wap-console-wrap .console-output .output-log{position:relative;padding-left:30px;line-height:2}.wap-console-wrap .console-output .input-log:before,.wap-console-wrap .console-output .output-log:before{content:'';position:absolute;top:0.5em;width:0.8em;height:0.8em;border:2px solid #097125;transform:rotate(45deg)}.wap-console-wrap .console-output .input-log:before{left:0;border-color:#097125 #097125 transparent transparent}.wap-console-wrap .console-output .output-log{border-bottom:1px solid rgba(9,113,37,0.5)}.wap-console-wrap .console-output .output-log:before{left:0.5em;border-color:transparent transparent #097125 #097125}.wap-console-wrap .console-output .output-log .undefined,.wap-console-wrap .console-output .output-log .null{opacity:0.5}.wap-console-wrap .console-output .output-log .number{color:purple}.wap-console-wrap .opt-wrap{padding:10px 0}.wap-console-wrap .opt-wrap .btn{padding:0 10px;font-size:1.2em;color:#fff;border:2px solid #097125;border-radius:6px;background:transparent;outline:0}\n";
+var wapConsoleStyle = "\n.wap-console-wrap{position:fixed;z-index:9999;padding:2vw;top:2vw;left:2vw;right:2vw;bottom:2vw;background:rgba(0,0,0,0.5);border:2px solid #097125;color:#fff;overflow:hidden;box-sizing:border-box}.wap-console-wrap *{margin:0;padding:0;box-sizing:border-box}.wap-console-wrap.min{width:2vw;height:2vw}.wap-console-wrap.hide{display:none}.wap-console-wrap .fr{float:right}.wap-console-wrap .console-input{display:block;padding:1vw;width:100%;height:300px;color:#fff;background:transparent;border:0;border-top:2px solid #097125;outline:0;resize:none}.wap-console-wrap .console-input::placeholder{color:#fff}.wap-console-wrap .console-output{max-height:80%;overflow-y:scroll}.wap-console-wrap .console-output::-webkit-scrollbar{width:4px}.wap-console-wrap .console-output .input-log,.wap-console-wrap .console-output .output-log{position:relative;padding-left:30px;line-height:2}.wap-console-wrap .console-output .input-log:before,.wap-console-wrap .console-output .output-log:before{content:'';position:absolute;top:0.5em;width:0.8em;height:0.8em;border:2px solid #097125;transform:rotate(45deg)}.wap-console-wrap .console-output .input-log:before{left:0;border-color:#097125 #097125 transparent transparent}.wap-console-wrap .console-output .output-log{border-bottom:1px solid rgba(9,113,37,0.5)}.wap-console-wrap .console-output .output-log:before{left:0.5em;border-color:transparent transparent #097125 #097125}.wap-console-wrap .console-output .output-log .undefined,.wap-console-wrap .console-output .output-log .null{opacity:0.5}.wap-console-wrap .console-output .output-log .number{color:purple}.wap-console-wrap .opt-wrap{padding:10px 0}.wap-console-wrap .opt-wrap .btn{padding:0 10px;font-size:1.2em;color:#fff;border:2px solid #097125;border-radius:6px;background:transparent;outline:0}\n";
 "use strict";
 
-var wapConsoleHTML = "\n<div class=\"console-output\" id=\"console-output\"></div>\n<div class=\"opt-wrap\">\n    <button class=\"btn run\">Run</button>\n    <button class=\"btn min\">Min</button>\n    <button class=\"btn prev\">Prev</button>\n    <button class=\"btn next\">Next</button>\n    <button class=\"btn close fr\">Close</button>\n    <button class=\"btn clear fr\">Clear</button>\n</div>\n<textarea class=\"console-input\" placeholder=\"input some code...\"></textarea>\n";
+var wapConsoleHTML = "\n<div class=\"console-output\" id=\"console-output\"></div>\n<div class=\"opt-wrap\">\n    <button class=\"btn run\">Run</button>\n    <button class=\"btn min\">Min</button>\n    <button class=\"btn prev\">\u2191</button>\n    <button class=\"btn next\">\u2193</button>\n    <button class=\"btn close fr\">Close</button>\n    <button class=\"btn clear fr\">Clear</button>\n</div>\n<textarea class=\"console-input\" placeholder=\"input some code...\"></textarea>\n";
 'use strict';
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
@@ -141,7 +141,7 @@ var WapConsole = function () {
     }, {
         key: 'prev',
         value: function prev() {
-            if (this.historyIndex === null) {
+            if (this.historyIndex === null && this.history.length > 0) {
                 this.historyIndex = this.history.length - 1;
             } else if (this.historyIndex > 0) {
                 this.historyIndex--;
@@ -191,6 +191,7 @@ var WapConsole = function () {
 
             content.innerHTML = WapConsole._createHtml.apply(WapConsole, args);
             this.output.appendChild(content);
+            this.output.scrollTop = this.output.scrollHeight;
         }
 
         /**
